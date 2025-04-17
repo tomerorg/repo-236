a5e1ae8367f3b3d7022a4043b4dcba1516f89a27
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-17 13:20:29 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="repo-236",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="Experimental web project using FastAPI and Pandas and Pytest in Python",
+    keywords="repo-236, python",
+    url="",
+)
